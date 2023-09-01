452e790385b24490ca0af5de70b05e38b234d78a
Adnane Abid <92402125+adnane84@users.noreply.github.com>
2023-09-01 11:55:57 -0400

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,519 @@
+const products = [
+  {
+    "_id": "64bc3dcbfc88ec95892b48bb",
+    "title": "Free Code Camp",
+    "image": "https://cdn.freecodecamp.org/platform/universal/fcc_meta_1920X1080-indigo.png",
+    "quantity": 1,
+    "link": "https://www.freecodecamp.org/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ],
+    "description": "freeCodeCamp (also referred to as Free Code Camp) is a non-profit organization [3] that consists of an interactive learning web platform, an online community forum, chat rooms, online publications and local organizations that intend to make learning web development accessible to anyone.",
+    "createdAt": "2023-07-22T20:36:27.962Z",
+    "updatedAt": "2023-08-01T14:43:37.317Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bc42e7fc88ec95892b48bf",
+    "title": "Coursera",
+    "image": "https://play-lh.googleusercontent.com/qq5__wITsoCx2kUK8TqVW2-8UDRuxET9kCzPzAPHad8umXiHRF2N0tZKuLezd0tiBQg",
+    "quantity": 1,
+    "link": "https://www.coursera.org/",
+    "type": [
+      "Subscription"
+    ],
+    "categories": [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "REACT",
+      "UI/UX design",
+      "Git",
+      "Figma",
+      "Python"
+    ],
+    "description": "Coursera is a global online learning platform that offers anyone, anywhere, access to online courses and degrees from leading universities and companies.",
+    "createdAt": "2023-07-22T20:58:15.692Z",
+    "updatedAt": "2023-08-01T14:46:45.094Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bc4e3025b802e840b0612e",
+    "title": "Udemy",
+    "image": "https://i.pcmag.com/imagery/reviews/05LfIOxtkYAZ4BIOXwOnZJs-24.fit_scale.size_1028x578.v1626447626.png",
+    "quantity": 1,
+    "link": "https://www.udemy.com/",
+    "type": [
+      "Subscription"
+    ],
+    "categories": [
+      "Web Development",
+      "HTML",
+      "CSS",
+      "Node.js",
+      "JavaScript",
+      "REACT",
+      "UI/UX design",
+      "Git",
+      "Figma"
+    ],
+    "description": "Udemy is a global online learning platform that offers anyone, anywhere, access to online courses and degrees from leading universities and companies.",
+    "createdAt": "2023-07-22T21:46:24.132Z",
+    "updatedAt": "2023-08-01T14:52:38.452Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bc512625b802e840b06130",
+    "title": "edx",
+    "image": "https://th.bing.com/th/id/R.35ffd2d59b8b6f1ff2e87f7eb3516284?rik=RQF1HhhL%2baiEtA&riu=http%3a%2f%2fdocs.edx.org%2fedx-docs%2fassets%2fimages%2flogo-edx.png&ehk=IyNA6s8wS7R13bRhVibA1hp2w2rZkCFMAUxSkROr0QM%3d&risl=&pid=ImgRaw&r=0",
+    "quantity": 1,
+    "link": "https://www.edx.org/",
+    "type": [
+      "Subscription"
+    ],
+    "categories": [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "REACT",
+      "UI/UX design",
+      "Git",
+      "Figma",
+      "Python"
+    ],
+    "description": "edX is an online learning destination and MOOC provider, offering high-quality courses from the world’s best universities and institutions to learners everywhere.",
+    "createdAt": "2023-07-22T21:59:02.267Z",
+    "updatedAt": "2023-08-01T14:47:20.394Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bc57028c444746f30df904",
+    "title": "Developer Mozilla",
+    "image": "https://download.logo.wine/logo/MDN_Web_Docs/MDN_Web_Docs-Logo.wine.png",
+    "quantity": 1,
+    "link": "https://developer.mozilla.org",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Web APIs"
+    ],
+    "description": "MDN Web Docs, previously Mozilla Developer Network and formerly Mozilla Developer Center, is a documentation repository and learning resource for web developers. It was started by Mozilla in 2005 as a unified place for documentation about open web standards, Mozilla's own projects, and developer guides.",
+    "createdAt": "2023-07-22T22:24:02.309Z",
+    "updatedAt": "2023-08-01T14:48:07.981Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bd219b2f34f1f7d7c53614",
+    "title": "A List Apart",
+    "image": "https://www.indiemakers.tools/media/images/alistapart.jpg",
+    "quantity": 1,
+    "link": "https://alistapart.com/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Web Design",
+      "Front-end Development",
+      "User Experience"
+    ],
+    "description": "A List Apart is an online magazine that publishes articles and insights on web design, front-end development, and user experience.",
+    "createdAt": "2023-07-23T12:48:27.762Z",
+    "updatedAt": "2023-07-23T23:16:02.022Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdaf3b29fde3c5a0dc35f2",
+    "title": "W3Schools",
+    "image": "https://logospng.org/download/w3schools/w3schools-1536.png",
+    "quantity": 1,
+    "link": "https://www.w3schools.com/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "SQL",
+      "PHP",
+      "Bootstrap"
+    ],
+    "description": "W3Schools is a popular web development learning website that offers tutorials and examples for HTML, CSS, JavaScript, SQL, PHP, and Bootstrap.",
+    "createdAt": "2023-07-23T22:52:44.021Z",
+    "updatedAt": "2023-07-23T23:19:04.700Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdaf6129fde3c5a0dc35f4",
+    "title": "Codecademy",
+    "image": "https://static3.makeuseofimages.com/wp-content/uploads/2014/04/codeacademy-logo.jpg",
+    "quantity": 1,
+    "link": "https://www.codecademy.com/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Web Design",
+      "Front-end Development",
+      "CSS"
+    ],
+    "description": "Codecademy is an interactive learning platform that offers coding courses for various programming languages and web development topics.",
+    "createdAt": "2023-07-23T22:53:21.420Z",
+    "updatedAt": "2023-08-01T14:36:16.134Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdaf6d29fde3c5a0dc35f6",
+    "title": "Stack Overflow",
+    "image": "https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png?v=c78bd457575a",
+    "quantity": 1,
+    "link": "https://stackoverflow.com/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Programming Q&A",
+      "Web Development"
+    ],
+    "description": "Stack Overflow is a question and answer platform where developers can seek help, share knowledge, and discuss various web development topics.",
+    "createdAt": "2023-07-23T22:53:33.142Z",
+    "updatedAt": "2023-07-23T22:53:33.142Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdaf7a29fde3c5a0dc35f8",
+    "title": "CSS-Tricks",
+    "image": "https://i1.wp.com/zao.is/wp-content/uploads/css-tricks-e1394062740966.png?ssl=1https://i1.wp.com/zao.is/wp-content/uploads/css-tricks-e1394062740966.pnghttps://i1.wp.com/zao.is/wp-content/uploads/css-tricks-e1394062740966.png",
+    "quantity": 1,
+    "link": "https://css-tricks.com/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "CSS",
+      "Web Design",
+      "Front-end Development"
+    ],
+    "description": "CSS-Tricks is a website dedicated to sharing tips, tricks, and tutorials related to CSS, web design, and front-end development.",
+    "createdAt": "2023-07-23T22:53:46.532Z",
+    "updatedAt": "2023-07-23T23:23:11.742Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdaf8b29fde3c5a0dc35fa",
+    "title": "Mozilla Hacks",
+    "image": "https://yt3.ggpht.com/-qnAj2NI_hX0/AAAAAAAAAAI/AAAAAAAAAAA/qrTbN5kBT-w/s900-c-k-no-mo-rj-c0xffffff/photo.jpg",
+    "quantity": 1,
+    "link": "https://hacks.mozilla.org/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Web Development",
+      "Open Web Standards"
+    ],
+    "description": "Mozilla Hacks is Mozilla's official blog for web developers, providing updates on web technologies and open web standards.",
+    "createdAt": "2023-07-23T22:54:03.216Z",
+    "updatedAt": "2023-07-23T23:24:33.351Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdaf9929fde3c5a0dc35fc",
+    "title": "Web.dev",
+    "image": "https://iconape.com/wp-content/png_logo_vector/web-dev.png",
+    "quantity": 1,
+    "link": "https://web.dev/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Web Development",
+      "Performance",
+      "SEO"
+    ],
+    "description": "Web.dev is a learning platform by Google that offers insights and best practices for web development, performance, and search engine optimization.",
+    "createdAt": "2023-07-23T22:54:17.490Z",
+    "updatedAt": "2023-07-23T23:26:16.822Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdafa729fde3c5a0dc35fe",
+    "title": "Smashing Magazine",
+    "image": "https://brolik.com/images/smashing-mag-logo.png",
+    "quantity": 1,
+    "link": "https://www.smashingmagazine.com/",
+    "type": [
+      "Free",
+      "Paid (eBooks)"
+    ],
+    "categories": [
+      "Front-end Development",
+      "UI/UX design",
+      "Web Design"
+    ],
+    "description": "Smashing Magazine is an online magazine that offers articles, tutorials, and eBooks on web design, front-end development, and UI/UX topics.",
+    "createdAt": "2023-07-23T22:54:31.988Z",
+    "updatedAt": "2023-08-01T14:54:28.943Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdafc029fde3c5a0dc3600",
+    "title": "Dev.to",
+    "image": "https://d2fltix0v2e0sb.cloudfront.net/dev-black.png",
+    "quantity": 1,
+    "link": "https://dev.to/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Programming",
+      "Web Development",
+      "Community"
+    ],
+    "description": "Dev.to is a community-driven platform where developers share their knowledge, experiences, and insights on various programming and web development topics.",
+    "createdAt": "2023-07-23T22:54:56.728Z",
+    "updatedAt": "2023-07-23T23:29:17.022Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdafcd29fde3c5a0dc3602",
+    "title": "The Net Ninja",
+    "image": "https://www.filepicker.io/api/file/JIGkr7PVQeuw9rcBtGuB",
+    "quantity": 1,
+    "link": "https://netninja.dev/",
+    "type": [
+      "Free",
+      "Paid (Courses)"
+    ],
+    "categories": [
+      "Web Development",
+      "Front-end Development",
+      "Back-end Development"
+    ],
+    "description": "The Net Ninja is a website that offers free and paid courses on web development, covering both front-end and back-end technologies.",
+    "createdAt": "2023-07-23T22:55:09.471Z",
+    "updatedAt": "2023-07-23T23:34:47.598Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb00f29fde3c5a0dc3604",
+    "title": "SitePoint",
+    "image": "https://anbilarabi.com/wp-content/uploads/2019/02/sitepoint-logo-new.png",
+    "quantity": 1,
+    "link": "https://www.sitepoint.com/",
+    "type": [
+      "Free",
+      "Paid (Courses, Books)"
+    ],
+    "categories": [
+      "Web Development",
+      "Front-end Development",
+      "Back-end Development"
+    ],
+    "description": "SitePoint offers a variety of web development resources, including articles, courses, and books, covering front-end and back-end topics.",
+    "createdAt": "2023-07-23T22:56:15.905Z",
+    "updatedAt": "2023-07-23T23:35:58.518Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb04c29fde3c5a0dc3606",
+    "title": "Scotch.io",
+    "image": "https://th.bing.com/th/id/OIP.GHWzz_CPJBYjxkgEm0D0XwHaHa?pid=ImgDet&rs=1",
+    "quantity": 1,
+    "link": "https://scotch.io/",
+    "type": [
+      "Free",
+      "Paid (Courses)"
+    ],
+    "categories": [
+      "Web Development",
+      "Node.js",
+      "JavaScript",
+      "REACT"
+    ],
+    "description": "Scotch.io provides tutorials and courses on web development, JavaScript, Node.js, React, and other technologies.",
+    "createdAt": "2023-07-23T22:57:16.650Z",
+    "updatedAt": "2023-08-01T14:50:36.280Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb05929fde3c5a0dc3608",
+    "title": "Frontend Masters",
+    "image": "https://frontendmasters.github.io/bootcamp/static/FrontendMastersLogo-f72cae0c73fecbb6beecea606d8fabd3-ad675.png",
+    "quantity": 1,
+    "link": "https://frontendmasters.com/",
+    "type": [
+      "Paid (Membership)"
+    ],
+    "categories": [
+      "Web Development",
+      "Front-end Development",
+      "JavaScript"
+    ],
+    "description": "Frontend Masters is a learning platform with in-depth courses on front-end development, JavaScript, and modern web technologies.",
+    "createdAt": "2023-07-23T22:57:29.598Z",
+    "updatedAt": "2023-07-23T23:40:25.682Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb06729fde3c5a0dc360a",
+    "title": "Google Developers",
+    "image": "https://pluspng.com/img-png/google-developers-logo-png-event-details-2480.png",
+    "quantity": 1,
+    "link": "https://developers.google.com/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Web Development",
+      "Mobile Development",
+      "Cloud"
+    ],
+    "description": "Google Developers provides resources, documentation, and tools for web developers, mobile app developers, and cloud-based solutions.",
+    "createdAt": "2023-07-23T22:57:43.492Z",
+    "updatedAt": "2023-07-23T23:44:40.222Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb07329fde3c5a0dc360c",
+    "title": "Codrops",
+    "image": "https://cdn.freebiesupply.com/logos/thumbs/2x/codrops-logo.png",
+    "quantity": 1,
+    "link": "https://tympanus.net/codrops/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Web Design",
+      "Front-end Development",
+      "CSS"
+    ],
+    "description": "Codrops is a web design and development blog that showcases creative CSS techniques, UI components, and web design inspiration.",
+    "createdAt": "2023-07-23T22:57:55.781Z",
+    "updatedAt": "2023-07-23T23:45:45.633Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb08029fde3c5a0dc360e",
+    "title": "Web Fundamentals (Google)",
+    "image": "https://developers.google.com/web/images/web-fundamentals-icon192x192.png",
+    "quantity": 1,
+    "link": "https://developers.google.com/web/fundamentals",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Web Development",
+      "Front-end Development",
+      "PWA"
+    ],
+    "description": "Web Fundamentals is a Google resource that covers essential web development topics, including Progressive Web Apps (PWA).",
+    "createdAt": "2023-07-23T22:58:08.206Z",
+    "updatedAt": "2023-07-23T22:58:08.206Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb08f29fde3c5a0dc3610",
+    "title": "CSS Grid Garden",
+    "image": "https://cssgridgarden.com/favicon.ico",
+    "quantity": 1,
+    "link": "https://cssgridgarden.com/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "CSS",
+      "Web Design",
+      "Interactive Learning"
+    ],
+    "description": "CSS Grid Garden is an interactive game that helps you learn CSS Grid layout through a fun and engaging coding challenge.",
+    "createdAt": "2023-07-23T22:58:23.977Z",
+    "updatedAt": "2023-07-23T22:58:23.977Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb09d29fde3c5a0dc3612",
+    "title": "Responsive Web Design Fundamentals (Google)",
+    "image": "https://developers.google.com/web/images/web-fundamentals-icon192x192.png",
+    "quantity": 1,
+    "link": "https://developers.google.com/web/fundamentals/design-and-ui/responsive/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Web Design",
+      "Responsive Design"
+    ],
+    "description": "Responsive Web Design Fundamentals is a Google guide that teaches the principles of designing responsive websites that adapt to different devices.",
+    "createdAt": "2023-07-23T22:58:37.980Z",
+    "updatedAt": "2023-07-23T22:58:37.980Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb0ab29fde3c5a0dc3614",
+    "title": "Can I use...",
+    "image": "https://caniuse.com/img/favicon-128.png",
+    "quantity": 1,
+    "link": "https://caniuse.com/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Web Development",
+      "Browser Support"
+    ],
+    "description": "Can I use... is a reference website that provides information about browser support for various web technologies and features.",
+    "createdAt": "2023-07-23T22:58:51.440Z",
+    "updatedAt": "2023-07-23T22:58:51.440Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb0b629fde3c5a0dc3616",
+    "title": "Serverless Stack",
+    "image": "https://www.opensourceforu.com/wp-content/uploads/2021/07/serverless-stacks-2.png",
+    "quantity": 1,
+    "link": "https://serverless-stack.com/",
+    "type": [
+      "Free"
+    ],
+    "categories": [
+      "Serverless",
+      "AWS",
+      "Web Development"
+    ],
+    "description": "Serverless Stack offers tutorials and guides on building serverless applications using AWS (Amazon Web Services).",
+    "createdAt": "2023-07-23T22:59:02.401Z",
+    "updatedAt": "2023-07-23T23:42:44.255Z",
+    "__v": 0
+  },
+  {
+    "_id": "64bdb0c329fde3c5a0dc3618",
+    "title": "CodePen",
+    "image": "https://nvision.co/wp-content/uploads/2020/06/codepen.png",
+    "quantity": 1,
+    "link": "https://codepen.io/",
+    "type": [
+      "Paid (Membership)"
+    ],
+    "categories": [
+      "Web Development",
+      "Front-end Development",
+      "Code Playground"
+    ],
+    "description": "CodePen is an online code editor and playground where developers can write, share, and collaborate on HTML, CSS, and JavaScript code snippets.",
+    "createdAt": "2023-07-23T22:59:15.696Z",
+    "updatedAt": "2023-08-01T14:38:17.928Z",
+    "__v": 0
+  }
+];
+
+export default products;
